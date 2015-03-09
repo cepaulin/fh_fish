aa9ac3025d37c227d6b42afdf61611153e988628
n1co <nico83110@gmail.com>
2015-03-09 18:47:49 +0100

    Create fish.lua

---
+++ fish.lua
@@ -0,0 +1,84 @@
+local PlayerGuid = UnitGUID("player")
+local PlayerObject = ObjectPointer("player")
+
+SLASH_fish = '/fish'
+
+local function GetLatency()
+	return select(4, GetNetStats())
+end
+
+local function Sleep(Interval)
+	FishUntil = GetTime() + (Interval / 1000)
+end
+
+local function spelltoName(spellID)
+	return tostring(select(1,GetSpellInfo(spellID)))
+end
+
+local function getBobber()
+	local BobberName = "Fishing Bobber"
+	local Total = ObjectCount(TYPE_GAMEOBJECT)
+	local BobberDescriptor = nil
+	
+	for i = 1, Total,1 do
+		local Object = ObjectWithIndex(i)
+		local ObjectName = ObjectName(ObjectPointer(Object))
+
+		if ObjectName == BobberName then
+				--print("bobber found !!")
+				--print(ObjectField(Object,0x1E0,SInt))
+				return Object
+		end
+
+	end
+end
+
+local function isBobbing()
+	local bobbing = ObjectField(getBobber(),0x1E0,SInt)
+	if bobbing == 1 then
+		return true
+	else
+		return false
+	end
+end
+
+local function goFish()
+	local BobberObject = getBobber()
+	if BobberObject then
+		if isBobbing() == true then
+			print("|cffff6060[|r|cFF00FFFFFishbot|cffff6060]|r I got a fish !")
+			ObjectInteract(getBobber())
+		else
+			--print("not bobbing")
+		end
+	else
+		CastSpellByName(spelltoName(131474))
+	end
+end
+
+
+local BobberFrame = CreateFrame("FRAME", "FishingFrame")
+ BobberFrame:SetScript("OnUpdate", function(self, elapsed)
+     if self.TimeSinceLastUpdate == nil then self.TimeSinceLastUpdate = 0 end
+     self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed
+     if (self.TimeSinceLastUpdate > 0.2) and enabled == 1 then
+		 goFish()
+         self.TimeSinceLastUpdate = 0
+   	 end
+ end)
+ 
+ SLASH_HELLOWORLD1, SLASH_HELLOWORLD2 = '/fish', '/fishbot';
+
+enabled = 0;
+
+function handler(msg, editbox)
+ if enabled == 1 then
+  print("|cffff6060[|r|cFF00FFFFFishbot|cffff6060]|r Disabled.");
+  enabled = 0;
+ else
+  print("|cffff6060[|r|cFF00FFFFFishbot|cffff6060]|r Enabled");
+  enabled = 1;
+ end
+end
+
+SlashCmdList["HELLOWORLD"] = handler;
